360049ff5a4dff2621a82558b999995956b0aa31
jgpro <kingjgpro@gmail.com>
2019-01-14 09:18:34 +1300

    Update Illuminat-Widget.sol

--- contracts/Illuminat-Widget.sol
+++ contracts/Illuminat-Widget.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.2;
+pragma solidity ^0.5.0;
 
 contract Owned {
     address public owner;
@@ -16,22 +16,23 @@ contract Owned {
     }
 }
 
+
 interface IERC20 {
-  function totalSupply() external view returns (uint256);
+    function totalSupply() external view returns (uint256);
 
-  function balanceOf(address who) external view returns (uint256);
-
-  function allowance(address owner, address spender)
+    function balanceOf(address who) external view returns (uint256);
+  
+    function allowance(address owner, address spender)
     external view returns (uint256);
 
-  function transfer(address to, uint256 value) external returns (bool);
+    function transfer(address to, uint256 value) external returns (bool);
 
-  function approve(address spender, uint256 value)
+    function approve(address spender, uint256 value)
     external returns (bool);
 
-  function transferFrom(address from, address to, uint256 value)
+    function transferFrom(address from, address to, uint256 value)
     external returns (bool);
-    
+
   event Transfer(
     address indexed from,
     address indexed to,
@@ -45,11 +46,11 @@ interface IERC20 {
   );
 }
 
-contract TokenVendor is IERC20, Owned {
+contract TokenVendor is Owned {
     uint256 public priceInWei = 40000000000000000;
     address public tokenAddress = 0x9F34Ad564c5Cc5137726Fca8fA87Ac44f7866F39;
     uint256 public tokenDecimals = 4;
-    
+
     // Constructor - Sets the token Owner
     constructor() public {
         owner = 0xc7a1Bd7a0A7eF23cb2544641CF6d7D14157A71bb;
@@ -79,6 +80,16 @@ contract TokenVendor is IERC20, Owned {
     function () external payable {
         require(msg.value >= priceInWei && msg.value > 0);
         require(IERC20(tokenAddress).allowance(owner,address(this)) >= (msg.value / priceInWei) /  10 ** tokenDecimals);
+        require(IERC20(tokenAddress).balanceOf(owner) >= (msg.value / priceInWei) /  10 ** tokenDecimals);
+        uint256 amount = ((msg.value / priceInWei) /  10 ** tokenDecimals);
+        IERC20(tokenAddress).transferFrom(owner, address(this), amount);
+        IERC20(tokenAddress).transfer(msg.sender, amount);
+        emit Buy(msg.sender, amount);
+    }
+    function buy() public payable {
+        require(msg.value >= priceInWei && msg.value > 0);
+        require(IERC20(tokenAddress).allowance(owner,address(this)) >= (msg.value / priceInWei) /  10 ** tokenDecimals);
+        require(IERC20(tokenAddress).balanceOf(owner) >= (msg.value / priceInWei) /  10 ** tokenDecimals);
         uint256 amount = ((msg.value / priceInWei) /  10 ** tokenDecimals);
         IERC20(tokenAddress).transferFrom(owner, address(this), amount);
         IERC20(tokenAddress).transfer(msg.sender, amount);
